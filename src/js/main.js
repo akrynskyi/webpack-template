fae7b44592ebaa6a940b6f5720c251393f327a70
Andrii Krinskiy <andrewkrinskiy@gmail.com>
2020-02-27 23:15:55 +0200

    initial build

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const section = document.getElementById("section");
+
+btn.addEventListener("click", () => {
+  section.classList.toggle("add-color");
+});
